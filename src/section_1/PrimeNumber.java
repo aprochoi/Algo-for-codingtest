a01a06b72a76b6426b9b458bb98dcd8c1c40d6a2
aprochoi <ym24319577@gmail.com>
2022-11-15 17:44:43 +0900

    Section-2 5.소수(에라토스테네스 체)

---
+++ src/section_1/PrimeNumber.java
@@ -0,0 +1,28 @@
+package section_1;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    static int solution(int n) {
+        int res = 0;
+        int[] arr = new int[n+1];
+
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                res++;
+                for (int j = i; j < arr.length; j+=i) {
+                    arr[j] = 1;
+                }
+            }
+        }
+
+        return res;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(solution(n));
+    }
+}
