b3df312043834a38785c504350c82d13509735c3
aprochoi <ym24319577@gmail.com>
2022-11-15 12:28:19 +0900

    Section-2 1.큰수 출력하기

---
+++ src/section_1/PrintBigNum.java
@@ -0,0 +1,33 @@
+package section_1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PrintBigNum {
+
+    static ArrayList<Integer> solution(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1]) {
+                list.add(arr[i]);
+            }
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int a : solution(arr)) {
+            System.out.print(a + " ");
+        }
+    }
+}
