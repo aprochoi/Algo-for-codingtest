ed95ce299e814bebb36ca5b12ed9a86a84fe96c1
aprochoi <ym24319577@gmail.com>
2022-11-18 16:44:33 +0900

    Section-2 10.봉우리

---
+++ src/section_2/MountainTop.java
@@ -0,0 +1,43 @@
+package section_2;
+
+import java.util.Scanner;
+
+public class MountainTop {
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    static int solution(int n, int[][] arr) {
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] >= arr[i][j]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag) res++;
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println(solution(n, arr));
+
+    }
+}
