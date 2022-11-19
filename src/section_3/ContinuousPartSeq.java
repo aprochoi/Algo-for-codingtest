ed9f41b08ce31188c035ccf37d813e15668f7805
aprochoi <ym24319577@gmail.com>
2022-11-19 17:22:50 +0900

    Section-3 4.연속부분수열(Two pointers, Sliding Windows 사용)

--- src/section_3/ContinuousPartSeq.java
+++ src/section_3/ContinuousPartSeq.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class ContinuousPartSeq {
 
     static int solution(int n, int m, int[] arr) {
-        int res = 0, sum = 0, lt = 0;
-
+        int res = 0;
+        int sum = 0;
+        int lt = 0;
         for (int rt = 0; rt < n; rt++) {
             sum += arr[rt];
-            if (sum == m) res++;
-            while (sum >= m) {
+            while (sum > m) {
                 sum -= arr[lt];
                 lt++;
-                if(sum == m) res++;
+            }
+            if (sum == m) {
+                res++;
             }
         }
 
