ca074c141ea57c5402997a91e926fa2a68946cd9
aprochoi <ym24319577@gmail.com>
2022-11-22 10:26:00 +0900

    Section-4 3.매출액의 종류(HashMap, Two Pointers 알고리즘, Sliding Window 알고리즘 사용);

---
+++ src/section_4/KindOfSales.java
@@ -0,0 +1,41 @@
+package section_4;
+
+import java.util.*;
+
+public class KindOfSales {
+
+    static ArrayList<Integer> solution(int n, int k, int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < k - 1; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        int lt = 0;
+        for (int rt = k - 1; rt < n; rt++) {
+            map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
+            list.add(map.size());
+            map.put(arr[lt], map.get(arr[lt]) - 1);
+            if (map.get(arr[lt]) == 0) {
+                map.remove(arr[lt]);
+            }
+            lt++;
+        }
+
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i : solution(n, k, arr)) {
+            System.out.print(i + " ");
+        }
+    }
+}
