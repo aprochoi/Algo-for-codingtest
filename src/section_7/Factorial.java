af8abb9b9c85e0500085ca5054d7f5a7d5cd1447
aprochoi <ym24319577@gmail.com>
2022-11-28 19:57:24 +0900

    Section-7 3.팩토리얼(재귀 사용)

---
+++ src/section_7/Factorial.java
@@ -0,0 +1,18 @@
+package section_7;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    static int DFS(int n) {
+        if(n == 1) return 1;
+
+        return n * (DFS(n - 1));
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(DFS(n));
+    }
+}
