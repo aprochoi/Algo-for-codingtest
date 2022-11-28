0413998a10be16d7bb82993cb1d126da68a74240
aprochoi <ym24319577@gmail.com>
2022-11-28 14:56:15 +0900

    Section-6 7.좌표정렬(compareTo 사용)

---
+++ src/section_6/XYSort.java
@@ -0,0 +1,36 @@
+package section_6;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class XYSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Point> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            list.add(new Point(x, y));
+        }
+        Collections.sort(list);
+        for(Point p : list) System.out.println(p.x + " " + p.y);
+    }
+}
+
+class Point implements Comparable<Point> {
+    public int x;
+    public int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if(this.x == o.x) return this.y - o.y;
+        else return this.x - o.x;
+    }
+}
