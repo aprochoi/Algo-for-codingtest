f4935556bc446852efc7221c234e02fd95e53206
aprochoi <ym24319577@gmail.com>
2022-11-28 14:01:46 +0900

    Section-6 5.중복확인(정렬 후 검색)

--- src/section_6/DuplicateCheck.java
+++ src/section_6/DuplicateCheck.java
@@ -1,2 +1,25 @@
-package section_6;public class DuplicateCheck {
+package section_6;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DuplicateCheck {
+
+    static String solution(int n, int[] arr) {
+        Arrays.sort(arr);
+        for (int i = 0; i < n-1; i++) {
+            if(arr[i] == arr[i+1]) return "D";
+        }
+
+        return "U";
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        System.out.println(solution(n, arr));
+    }
 }
