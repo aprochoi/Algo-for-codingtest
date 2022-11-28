27debe14d705433fa0dbc4c927cdb37703df0dc7
aprochoi <ym24319577@gmail.com>
2022-11-28 17:35:39 +0900

    Section-6 9.뮤직비디오(결정알고리즘 사용)

---
+++ src/section_6/MusicVideo.java
@@ -0,0 +1,42 @@
+package section_6;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MusicVideo {
+
+    static int count(int[] arr, int capacity) {
+        int cnt = 1;
+        int sum = 0;
+        for (int i : arr) {
+            if(sum + i > capacity) {
+                cnt++;
+                sum=i;
+            } else sum += i;
+        }
+        return cnt;
+    }
+
+    static int solution(int n, int m, int[] arr) {
+        int res = 0;
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) <= m) {
+                res = mid;
+                rt = mid - 1;
+            } else lt = mid + 1;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        System.out.println(solution(n, m, arr));
+    }
+}
