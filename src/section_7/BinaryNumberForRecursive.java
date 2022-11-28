43ed5cb9c8baff3b33c3ad5c6cfe4eccdc41099c
aprochoi <ym24319577@gmail.com>
2022-11-28 19:23:51 +0900

    Section-7 2.이진수출력(재귀)

---
+++ src/section_7/BinaryNumberForRecursive.java
@@ -0,0 +1,17 @@
+package section_7;
+
+import java.util.Scanner;
+
+public class BinaryNumberForRecursive {
+
+    static void DFS(int n) {
+        if(n == 0) return;
+        DFS(n / 2);
+        System.out.print(n % 2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        DFS(n);
+    }
+}
